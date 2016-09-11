f7d039ff3f1439f10a7fdc899250631634a67876
Steve Konves <stephen.konves@gmail.com>
2016-09-10 21:55:12 -0700

    Fix coverage script

--- coverage.cmd
+++ coverage.cmd
@@ -1,5 +1,5 @@
 nuget install OpenCover -Version 4.6.519 -OutputDirectory tools
 nuget install coveralls.net -Version 0.412.0 -OutputDirectory tools
 
-.\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"vstest.console.exe" -filter:"+[*]* -[*.*Tests]*" -excludebyfile:"*.g.cs" -targetargs:".\tests\Konves.TextGraph.UnitTests\bin\Release\Konves.TextGraph.UnitTests.dll .\tests\Konves.TextGraph.UnitTests\bin\Release\Konves.TextGraph.HtmlParser.UnitTests.dll .\tests\Konves.TextGraph.UnitTests\bin\Release\Konves.TextGraph.MarkdownParser.UnitTests.dll" -register:user
+.\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"vstest.console.exe" -filter:"+[*]* -[*.*Tests]*" -excludebyfile:"*.g.cs" -targetargs:".\tests\Konves.TextGraph.UnitTests\bin\Release\Konves.TextGraph.UnitTests.dll .\tests\Konves.TextGraph.HtmlParser.UnitTests\bin\Release\Konves.TextGraph.HtmlParser.UnitTests.dll .\tests\Konves.TextGraph.MarkdownParser.UnitTests\bin\Release\Konves.TextGraph.MarkdownParser.UnitTests.dll" -register:user
 .\tools\coveralls.net.0.412\tools\csmacnz.Coveralls.exe --opencover -i .\results.xml
