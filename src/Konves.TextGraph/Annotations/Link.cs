363af72a5a3d15d5b4f7914b054009d61b13ee48
Steve Konves <stephen.konves@gmail.com>
2016-09-12 21:23:34 -0700

    Add equality checks to Link annotation

--- src/Konves.TextGraph/Annotations/Link.cs
+++ src/Konves.TextGraph/Annotations/Link.cs
@@ -14,5 +14,15 @@ namespace Konves.TextGraph.Annotations
 		public override string Subtype { get { return "link"; } }
 
 		public override string Type { get { return "navigation"; } }
+
+		public override bool Equals(object obj)
+		{
+			return base.Equals(obj) && (obj as Link).Uri == Uri;
+		}
+
+		public override int GetHashCode()
+		{
+			return GetHashCode(GetHashCode(), Uri);
+		}
 	}
 }
